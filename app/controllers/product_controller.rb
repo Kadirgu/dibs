e2d4b03cb32531a3441e94439355e05568ac1e36
gurcan <kadir.guercan@gmail.com>
2022-11-29 15:15:22 +0100

    first commit

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -7,25 +7,24 @@ class ProductController < ApplicationController
     else
       @product = Product.all
     end
-    @markers = @friend_services.geocoded.map do |friend_service|
+    @markers = @products.geocoded.map do |product|
       {
-        lat: friend_service.latitude,
-        lng: friend_service.longitude,
-        info_window: render_to_string(partial: "info_window", locals: {friend_service: friend_service})
+        lat: product.latitude,
+        lng: product.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {product: friend_service})
       }
     end
   end
 
-    def show
-      @product = Prodcut.find(params[:id])
-      @product = Product.new
-      @user = @product.user
-    end
+  def show
+    @product = Prodcut.find(params[:id])
+    @product = Product.new
+    @user = @product.user
+  end
 
-    def new
-      @product = Product.new
-
-    end
+  def new
+    @product = Product.new
+  end
 
   def create
     @product = Product.new(product_params)
@@ -36,11 +35,11 @@ class ProductController < ApplicationController
       render :new, status: :unprocessable_entity
   end
 
-    def update
-      @product = Product.find(params[:id])
-      @product.update(product_params)
-      redirect_to product_path
-    end
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path
+  end
 
     def destroy
       @product = Product.find(params[:id])
