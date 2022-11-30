41ee47cba5427be99383d740b7c09dd2d8096b39
michellerodriguez <michellerzevada@gmail.com>
2022-11-30 14:20:29 +0100

    search

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,13 @@ class Product < ApplicationRecord
   # geocoded_by :location
   # after_validation :geocode, if: :will_save_change_to_location?
 
+  include PgSearch::Model
+  pg_search_scope :search_a_lot,
+    against: [ :location, :title, :description, :image, :price, :material, :condition, :user_id ],
+    using: {
+      tsearch: { prefix: true }
+  }
 end
-
-
 
 # cheat-sheet:
 
@@ -15,10 +19,3 @@ end
 # has_one_attached :photo
 # geocoded_by :location
 # after_validation :geocode, if: :will_save_change_to_location?
-
-# include PgSearch::Model
-# pg_search_scope :search_a_lot,
-# against: [ :location, :title, :description, :category, :interests, :age, :gender ],
-# using: {
-#   tsearch: { prefix: true }
-# }
