4f652135df02a47a530aac867735b047ee0d58db
gurcan <kadir.guercan@gmail.com>
2022-11-30 17:55:12 +0100

    1

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,19 @@
 class ChatsController < ApplicationController
 
   def index
+    @chats = Chat.all
   end
 
   def show
+    @chat = Chat.find(params[:id])
+    @user = @product.chat
   end
 
   def destroy
+    @chat = Chat.find(params[:id])
+    @chat.destroy
+    flash[:success] = "The chat was successfully deleted."
+    redirect_to chats_path
   end
 
 end
