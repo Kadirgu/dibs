77b0c3c58984590de7ea79c3833f9ab9ab2bafb4
gurcan <kadir.guercan@gmail.com>
2022-12-01 12:21:11 +0100

    add modedls

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,22 +1,4 @@
 class Message < ApplicationRecord
-  has_many :user, dependent: :destroy
-  belongs to :user
+  belongs_to :user
+  belongs to :chatroom, dependent: :destroy
 end
-
-
-
-
-# cheat-sheet:
-
-# has_many :bookings, dependent: :destroy
-# belongs_to :user
-# has_one_attached :photo
-# geocoded_by :location
-# after_validation :geocode, if: :will_save_change_to_location?
-
-# include PgSearch::Model
-# pg_search_scope :search_a_lot,
-# against: [ :location, :title, :description, :category, :interests, :age, :gender ],
-# using: {
-#   tsearch: { prefix: true }
-# }
