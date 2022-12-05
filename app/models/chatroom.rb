66f6e5265707426135c465f64e4ebc21364dcc50
Jasper Klose <klosejasper@gmail.com>
2022-12-05 17:30:52 +0100

    fix bugs

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,5 @@
 class Chatroom < ApplicationRecord
   belongs_to :product, dependent: :destroy
-  belongs_to :user 
-  has_many :messages
+  belongs_to :user
+  has_many :messages, dependent: :destroy
 end
