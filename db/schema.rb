70af3d0f1b12e833c3c650af101883656e8d5455
michellerodriguez <michellerzevada@gmail.com>
2022-12-06 16:42:30 +0100

    spotted changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_105504) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_152120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_105504) do
     t.float "latitude"
     t.float "longitude"
     t.string "category"
+    t.integer "status"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
