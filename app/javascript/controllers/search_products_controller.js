d032020d0bc57c410ce3119d0f7359044bb806a9
Jasper Klose <klosejasper@gmail.com>
2022-12-06 11:24:28 +0100

    formwith search

---
+++ app/javascript/controllers/search_products_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search-products"
+export default class extends Controller {
+  connect() {
+  }
+
+  update() {
+    document.getElementById("searchForm").submit();
+  }
+}
